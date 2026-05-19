7c42063fb4a3ca4dc80a3c3924258c3b4a98f026
jacob <wogur2590@naver.com>
2026-05-19 23:17:11 +0900

    test

--- infra/deployment/terraform/tailscale.tf
+++ infra/deployment/terraform/tailscale.tf
@@ -8,15 +8,15 @@ resource "tailscale_tailnet_key" "ccmall_join_key" {
 }
 
 # --- Terraform이 기기를 찾고 라우팅 승인
-#data "tailscale_device" "rec_device" {
+data "tailscale_device" "rec_device" {
   #OS hostname Rec을 가져다 디바이스 생성함.
- # hostname   = "ccmall-rec"
-  #wait_for   = "300s"
-  #depends_on = [terraform_data.bootstrap_user1]
-#}
+  hostname   = "ccmall-rec"
+  wait_for   = "300s"
+  depends_on = [terraform_data.bootstrap_user1]
+}
 
-#resource "tailscale_device_subnet_routes" "approve_vpc_routes" {
- # device_id = data.tailscale_device.rec_device.id
-  #routes    = [aws_vpc.ccmall_vpc.cidr_block]
-#}
+resource "tailscale_device_subnet_routes" "approve_vpc_routes" {
+  device_id = data.tailscale_device.rec_device.id
+  routes    = [aws_vpc.ccmall_vpc.cidr_block]
+}
 
