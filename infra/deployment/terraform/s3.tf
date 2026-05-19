63ec501dcdc851cc0d7a22a96315b9c59d9dc9dc
jacob <wogur2590@naver.com>
2026-05-19 23:05:42 +0900

    test key

--- infra/deployment/terraform/s3.tf
+++ infra/deployment/terraform/s3.tf
@@ -13,6 +13,7 @@ data "aws_s3_bucket" "ccmall_bucket" {
 resource "aws_iam_role" "ec2_s3_role" {
   name = "EC2-S3-ACCESS-ROLE"
 
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
